d22df6f120b67d6f842c6bda642f71aa8ba812a3
mallela009 <92942717+mallela009@users.noreply.github.com>
2022-05-26 14:39:07 +0000

    third commit

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+public  class helloworld {
+    public static void main(String[] args){
+
+        System.out.println("hello world java core");
+        system.out.println("hi java girlfriend i love you");
+        System.out.println("this is my java program");
+        
+    }
+    
+}
