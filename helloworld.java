c8530bb1e6254a84eb32cd82595921ee448bf81d
mallela009 <92942717+mallela009@users.noreply.github.com>
2022-05-26 11:30:11 +0000

    second commit

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,7 @@ public  class helloworld {
     public static void main(String[] args){
 
         System.out.println("hello world java core");
+        system.out.println("hi java girlfriend i love you");
     }
     
 }
